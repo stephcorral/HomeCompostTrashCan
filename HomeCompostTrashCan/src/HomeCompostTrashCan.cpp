4d63e678e6851eac78925f6320a9d66c42e28a65
stephanie corral <stephanie.corral8@gmail.com>
2025-04-08 14:40:38 -0600

    added weight libray

--- HomeCompostTrashCan/src/HomeCompostTrashCan.cpp
+++ HomeCompostTrashCan/src/HomeCompostTrashCan.cpp
@@ -1,35 +1,29 @@
 /* 
- * Project myProject
- * Author: Your Name
- * Date: 
+ * Home Compost Trash Can
+ * Author: Stephanie Corral
+ * Date: 4-8-2025
+ * 
+ * Air Quality
+ * Load Cell
+ * motors
+ * fans
+ * 
  * For comprehensive documentation and examples, please visit:
  * https://docs.particle.io/firmware/best-practices/firmware-template/
  */
 
 // Include Particle Device OS APIs
 #include "Particle.h"
+#include "HX711.h"
 
-// Let Device OS manage the connection to the Particle Cloud
 SYSTEM_MODE(AUTOMATIC);
 
-// Run the application and system concurrently in separate threads
 SYSTEM_THREAD(ENABLED);
 
-// Show system, cloud connectivity, and application logs over USB
-// View logs with CLI using 'particle serial monitor --follow'
 SerialLogHandler logHandler(LOG_LEVEL_INFO);
 
-// setup() runs once, when the device is first turned on
 void setup() {
-  // Put initialization like pinMode and begin functions here
 }
 
-// loop() runs over and over again, as quickly as it can execute.
 void loop() {
-  // The core of your code will likely live here.
-
-  // Example: Publish event to cloud every 10 seconds. Uncomment the next 3 lines to try it!
-  // Log.info("Sending Hello World to the cloud!");
-  // Particle.publish("Hello world!");
-  // delay( 10 * 1000 ); // milliseconds and blocking - see docs for more info!
 }
