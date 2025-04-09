5d190b0658b6c5073b7f868628daa80046f7821c
stephanie corral <stephanie.corral8@gmail.com>
2025-04-09 16:37:56 -0600

    Updated gitignore

--- HomeCompostTrashCan/src/HomeCompostTrashCan.cpp
+++ HomeCompostTrashCan/src/HomeCompostTrashCan.cpp
@@ -14,20 +14,22 @@
 
 // Include Particle Device OS APIs
 #include "Particle.h"
+#include <Adafruit_MQTT.h>
 #include "HX711.h"
-#include "Adafruit_MQTT/Adafruit_MQTT_SPARK.h"
-#include "Adafruit_MQTT/Adafruit_MQTT.h"
+#include "Adafruit_MQTT_SPARK.h"
 #include "credentials.h"
 
 //create TCP Client
 TCPClient TheClient;
 //setup MQTT Server and login
-Adafruit_MQTT_SPARK mqtt(&TheClient, AIO_SERVERPORT,AIO_USERNAME, AIO_KEY);
+Adafruit_MQTT_SPARK mqtt(&TheClient,AIO_SERVER, AIO_SERVERPORT,AIO_USERNAME, AIO_KEY);
 //Feeds
-Adafruit_MQTT_Publish pubFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "feeds/trashdrawer");
+Adafruit_MQTT_Publish pubWeight = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/trashdrawer");
+Adafruit_MQTT_Publish pubAlert = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/alertdrawer");
 //variables for publish weight feed
 unsigned int last, lastTime;
 float pubValue;
+String greenAlert, yellowAlert, orangeAlert, redAlert; 
 //functions for publishing weight
 void MQTT_connect();
 bool MQTT_ping();
@@ -48,7 +50,8 @@ SerialLogHandler logHandler(LOG_LEVEL_INFO);
 void setup() {
     Serial.begin(9600);
     waitFor(Serial.isConnected, 10000);
-
+    
+    WiFi.on();
     WiFi.connect();
     while (WiFi.connecting())
     {
@@ -63,8 +66,10 @@ void setup() {
 }
 
 void loop() {
+    MQTT_connect();
+    MQTT_ping();
     //setting up pub for weight
-    if()
+
     //setting up weight load cell for drainage drawer
     //flow will be if drawer is at a certain weight 
     //alert will be sent to phone and after a certain time
@@ -74,6 +79,38 @@ void loop() {
     calibration =myScale.get_scale();
 
     Serial.printf("Weight = %0.02f\r", weight);
+    
+    //setting up pub for weight
+    if((millis()-lastTime)>6000){
+        if(mqtt.Update()){
+          pubWeight.publish(weight);
+            Serial.printf("Weight Sent to Adafruit.io = %0.0f\n",weight);
+        }
+        lastTime = millis();
+        
+        if(weight <300){
+        greenAlert = "<<< Ready for Compost >>>";
+        pubAlert.publish(greenAlert);
+        Serial.println(greenAlert);
+        //set light color
+        }
+        if(weight>300 && weight <500){
+          yellowAlert = "<<< Half Way Full >>>";
+          pubAlert.publish(yellowAlert);
+          Serial.println(yellowAlert);
+        }
+        if(weight>500 && weight<600){
+          orangeAlert = "<<< Almost Full >>>";
+          pubAlert.publish(orangeAlert);
+          Serial.println(orangeAlert);
+        }
+        if(weight>600){
+          redAlert = "<<<FULL!!! Empty Out Drawer Please>>>";
+          pubAlert.publish(redAlert);
+          Serial.println(redAlert);
+        }
+    }
+
 }
 // Function to connect and reconnect as necessary to the MQTT server.
 // Should be called in the loop function and it will take care if connecting.
